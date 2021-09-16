f7a5db7ed0577933fd118ee41ea7118579eca1ef
Ulises Ponce Mendoza <upmendoza@gmail.com>
2021-09-16 14:29:31 -0700

    Revert "Commit Inicial"
    This reverts commit 5a24e3ca4a981f6a23938ee1e8ba93f46485cd48.

--- Bitacora/start.bat
+++
@@ -1,7 +0,0 @@
-echo ********************************************
-echo * INICIANDO BITACORA DEL CENTRO DE COMPUTO *
-echo ********************************************
-echo ============== BIENVENIDO ==================
-
-@echo off
-start javaw -Xmx200m -jar "c:\users\a123\appdata\local\Bitacora\dist\Bloqueo.jar"
